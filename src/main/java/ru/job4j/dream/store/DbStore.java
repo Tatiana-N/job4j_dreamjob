ff64c25d1fbc178a15d02618563281273bef2f47
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2021-12-21 07:57:30 +0300

    1. DbStore. [#281226] + логгирование

--- src/main/java/ru/job4j/dream/store/DbStore.java
+++ src/main/java/ru/job4j/dream/store/DbStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.store;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import ru.job4j.dream.Prop;
 import ru.job4j.dream.model.Candidate;
@@ -13,7 +14,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class DbStore implements Store {
+	
+	private static final String ERROR = "ошибка при выполнении запроса к базе данных";
 	
 	private final BasicDataSource pool = new BasicDataSource();
 	
@@ -29,6 +33,7 @@ public class DbStore implements Store {
 	
 	private static final class Lazy {
 		private static final Store INST = new DbStore();
+
 	}
 	
 	public static Store instOf() {
@@ -37,28 +42,30 @@ public class DbStore implements Store {
 	
 	public Collection<Post> findAllPosts() {
 		List<Post> posts = new ArrayList<>();
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("SELECT * FROM post")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("SELECT * FROM post")) {
 			try (ResultSet it = ps.executeQuery()) {
 				while (it.next()) {
 					posts.add(new Post(it.getInt("id"), it.getString("name")));
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 		return posts;
 	}
 	
 	public Collection<Candidate> findAllCandidates() {
 		List<Candidate> candidates = new ArrayList<>();
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidates")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidates")) {
 			try (ResultSet it = ps.executeQuery()) {
 				while (it.next()) {
 					candidates.add(new Candidate(it.getInt("id"), it.getString("name")));
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 		return candidates;
 	}
@@ -82,17 +89,21 @@ public class DbStore implements Store {
 	}
 	
 	private void update(Candidate candidate) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("UPDATE candidates SET name = (?) where id = (?)")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("UPDATE candidates SET name = (?) where id = (?)")) {
 			ps.setString(1, candidate.getName());
 			ps.setInt(2, candidate.getId());
 			ps.execute();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 	}
 	
 	private Candidate create(Candidate candidate) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("INSERT INTO candidates(name) VALUES (?)", PreparedStatement.RETURN_GENERATED_KEYS)) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement(
+		     		"INSERT INTO candidates(name) VALUES (?)", 
+				     PreparedStatement.RETURN_GENERATED_KEYS)) {
 			ps.setString(1, candidate.getName());
 			ps.execute();
 			try (ResultSet id = ps.getGeneratedKeys()) {
@@ -101,13 +112,16 @@ public class DbStore implements Store {
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 		return candidate;
 	}
 	
 	private Post create(Post post) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("INSERT INTO post(name) VALUES (?)", PreparedStatement.RETURN_GENERATED_KEYS)) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement(
+		     		"INSERT INTO post(name) VALUES (?)", 
+				     PreparedStatement.RETURN_GENERATED_KEYS)) {
 			ps.setString(1, post.getName());
 			ps.execute();
 			try (ResultSet id = ps.getGeneratedKeys()) {
@@ -116,23 +130,25 @@ public class DbStore implements Store {
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 		return post;
 	}
 	
 	private void update(Post post) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("UPDATE post SET name = (?) where id = (?)")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("UPDATE post SET name = (?) where id = (?)")) {
 			ps.setString(1, post.getName());
 			ps.setInt(2, post.getId());
 			ps.execute();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 	}
 	
 	public Post findByIdPost(Integer id) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("SELECT * FROM post WHERE id = ?")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("SELECT * FROM post WHERE id = ?")) {
 			ps.setInt(1, id);
 			try (ResultSet it = ps.executeQuery()) {
 				if (it.next()) {
@@ -140,7 +156,7 @@ public class DbStore implements Store {
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 		return null;
 	}
@@ -156,21 +172,23 @@ public class DbStore implements Store {
 	
 	@Override
 	public void deletePost(Integer id) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("DELETE FROM post WHERE id = (?)")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("DELETE FROM post WHERE id = (?)")) {
 			ps.setInt(1, id);
 			ps.execute();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 	}
 	
 	@Override
 	public void deleteCandidate(Integer id) {
-		try (Connection cn = pool.getConnection(); PreparedStatement ps = cn.prepareStatement("DELETE FROM candidates WHERE id = (?)")) {
+		try (Connection cn = pool.getConnection(); 
+		     PreparedStatement ps = cn.prepareStatement("DELETE FROM candidates WHERE id = (?)")) {
 			ps.setInt(1, id);
 			ps.execute();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(ERROR, e);
 		}
 	}
 }
