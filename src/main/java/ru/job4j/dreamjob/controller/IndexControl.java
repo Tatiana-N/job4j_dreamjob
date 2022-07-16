d1b26b0fc97aa87000c4a304aa36e635a6e7b110
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2022-07-16 13:17:52 +0300

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,13 +1,17 @@
 package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * @RestController (является сочетанием аннотаций @Controller и @ResponseBody) на
+ * @Controller (обозначает что данный класс является котроллером в модели Spring MVC).
+ */
+@Controller
 public class IndexControl {
 	
 	@GetMapping("/index")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "index";
 	}
 }
