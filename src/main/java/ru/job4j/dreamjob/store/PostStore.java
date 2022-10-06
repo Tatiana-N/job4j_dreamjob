5868c3e9bff6d9cb9153c62cef4ce9f5572b3dd1
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2022-10-06 19:45:18 +0300

    рефакторинг

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,9 +14,9 @@ public class PostStore {
 	private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 	
 	private PostStore() {
-		posts.put(1, new Post(1, "Junior Java Job"));
-		posts.put(2, new Post(2, "Middle Java Job"));
-		posts.put(3, new Post(3, "Senior Java Job"));
+		posts.put(1, new Post(1, "Junior Java Job", "сопляк", LocalDate.of(2022, 10, 1)));
+		posts.put(2, new Post(2, "Middle Java Job", "нормальный такой", LocalDate.of(2022, 10, 4)));
+		posts.put(3, new Post(3, "Senior Java Job", "уф профессор", LocalDate.of(2022, 10, 6)));
 	}
 	
 	public static PostStore instOf() {
