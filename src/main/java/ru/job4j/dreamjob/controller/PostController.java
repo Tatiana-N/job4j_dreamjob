0995d8f79db6905b66c07b673b50028e7dee22b9
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2022-10-08 08:01:42 +0300

    4. PostController.updatePost. Редактирование вакансии. [#504850]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
@@ -30,6 +31,19 @@ public class PostController {
 	public String createPost(@ModelAttribute Post post) {
 		post.setCreated(LocalDate.now());
 		store.add(post);
+		return "redirect:/posts";
+	}
+	
+	
+	@GetMapping("/formUpdatePost/{postId}")
+	public String formUpdatePost(@PathVariable("postId") int postId, Model model) {
+		model.addAttribute("post", store.findById(postId));
+		return "updatePost";
+	}
+	
+	@PostMapping("/updatePost")
+	public String updatePost(@ModelAttribute Post post) {
+		store.updatePost(post);
 		return "redirect:/posts";
 	}
 }
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -14,11 +14,12 @@ public class PostStore {
 	
 	private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 	private final AtomicInteger id;
+	
 	private PostStore() {
 		posts.put(1, new Post(1, "Junior Java Job", "сопляк", LocalDate.of(2022, 10, 1)));
 		posts.put(2, new Post(2, "Middle Java Job", "нормальный такой", LocalDate.of(2022, 10, 4)));
 		posts.put(3, new Post(3, "Senior Java Job", "уф профессор", LocalDate.of(2022, 10, 6)));
-		id = new AtomicInteger(posts.size());
+		id = new AtomicInteger(posts.size() + 1);
 	}
 	
 	public static PostStore instOf() {
@@ -29,10 +30,19 @@ public class PostStore {
 		return posts.values();
 	}
 	
+	public Post findById(int id) {
+		return posts.get(id);
+	}
+	
 	public Post add(Post post) {
 		int id = this.id.getAndIncrement();
 		post.setId(id);
 		posts.put(id, post);
 		return post;
+	}
+	
+	public Post updatePost(Post post) {
+		post.setCreated(LocalDate.now());
+		return posts.replace(post.getId(), post);
 	}
 }
